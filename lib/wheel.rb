9ddfce1e1995d3f87056af8f6daf47b850272e9f
Kuniaki Ishikawa <k.ishikawa.0128@gmail.com>
2019-09-07 23:20:16 +0900

    add circumference method to wheel class

--- lib/wheel.rb
+++ lib/wheel.rb
@@ -10,4 +10,8 @@ class Wheel
   def diameter
     rim + (tire * 2)
   end
+
+  def circumference
+    diameter * Math::PI
+  end
 end
--- spec/wheel_spec.rb
+++ spec/wheel_spec.rb
@@ -4,12 +4,18 @@ require_relative '../lib/wheel.rb'
 
 RSpec.describe Wheel do
   let(:wheel) { Wheel.new(rim, tire) }
+  let(:rim) { 26 }
+  let(:tire) { 1.5 }
 
   describe '#diameter' do
     subject { wheel.diameter }
-    let(:rim) { 26 }
-    let(:tire) { 1.5 }
 
     it { is_expected.to eq 29.0 }
+  end
+
+  describe '#circumference' do
+    subject { wheel.circumference }
+
+    it { is_expected.to eq 91.106186954104 }
   end
 end
